8082b2b885e10fd3ab72a8692d9fce34affcc9b4
Vladislav Klimenko <sunsingerus@gmail.com>
2018-04-12 15:39:54 +0300

    pbuilder - stretch

--- build_pbuilder.sh
+++ build_pbuilder.sh
@@ -22,3 +22,6 @@ ls -lh /var/cache/pbuilder/xenial-amd64/result/*.deb
 sudo DIST=trusty pbuilder create --configfile debian/.pbuilderrc && DIST=trusty pdebuild --configfile debian/.pbuilderrc
 ls -lh /var/cache/pbuilder/trusty-amd64/result/*.deb
 
+# deb 9 - stretch
+sudo DIST=stretch pbuilder create --configfile debian/.pbuilderrc && DIST=stretch pdebuild --configfile debian/.pbuilderrc
+ls -lh /var/cache/pbuilder/stable-amd64/result/*.deb
