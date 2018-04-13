6a7d4bee2ebaa7c0a98f7dbc1bca0213a84e26d3
Vladislav Klimenko <sunsingerus@gmail.com>
2018-04-13 13:28:36 +0300

    add automated repo list modification

--- build_deb9_stretch_brebones.sh
+++ build_deb9_stretch_brebones.sh
@@ -8,6 +8,7 @@
 #
 # deb http://deb.debian.org/debian experimental main
 # deb http://deb.debian.org/debian testing main
+sudo bash -c 'echo "deb http://deb.debian.org/debian testing main" >> /etc/apt/sources.list'
 
 sudo apt-get update
 sudo apt-get install -y gcc-7 g++-7
